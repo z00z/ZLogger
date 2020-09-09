c867ce34f1b2c3a9cbe6555f64cd664d525f336e
Zaid Al-Quraishi <zaid@isecur1ty.org>
2020-09-09 18:00:36 +0100

    Update install.sh
    Fixed to work with Kali 2020 - Thanks Diego 👍

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ rm /var/cache/apt/archives/lock
 rm /var/lib/apt/lists/lock
 sudo dpkg --add-architecture i386
 sudo apt-get update
+sudo apt-get install -y aptitude
+sudo aptitude install -y wine
 sudo apt-get install -y wine32 python-pip pyinstaller
 wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec /i python-2.7.14.msi
-sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller pynput
+sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6 pynput==1.6.8
 sudo pip install pynput
