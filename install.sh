630e738e485fd0d344075c746d9a28c86fce12c8
Zaid Al-Quraishi <zaid@isecur1ty.org>
2020-10-14 15:48:42 +0100

    Update install.sh
    Fixed Linux compatibility

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install -y aptitude
 sudo aptitude install -y wine
-sudo apt-get install -y wine32 python-pip pyinstaller
+sudo apt-get install -y wine32 python-pip 
 wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec /i python-2.7.14.msi
 sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6 pynput==1.6.8
-sudo pip install pynput
+sudo pip install pynput==1.6.8 pyinstaller==3.3.1
