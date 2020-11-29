8e19d3f647bd8d7807f4eaaa45159d81f57d5fe3
Zaid Al-Quraishi <zaid@isecur1ty.org>
2020-11-29 17:47:48 +0000

    Update install.sh
    Fixed dependancy issues

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ rm /var/cache/apt/archives/lock
 rm /var/lib/apt/lists/lock
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install -y aptitude
-sudo aptitude install -y wine
-sudo apt-get install -y wine32 python-pip 
+sudo apt-get install -y wine python-pip 
 wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec /i python-2.7.14.msi
 sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6 pynput==1.6.8
 sudo pip install pynput==1.6.8 pyinstaller==3.3.1
+apt install -y gnome-shell-extension-dashtodock
