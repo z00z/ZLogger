9d95147106668828160d1bea95ce39c9d78faab0
Zaid Al-Quraishi <zaid@isecur1ty.org>
2021-05-17 16:20:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ rm /var/cache/apt/archives/lock
 rm /var/lib/apt/lists/lock
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install -y wine python-pip 
+sudo apt-get install -y wine  
 wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec /i python-2.7.14.msi
+sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pefile==2019.4.18
 sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6 pynput==1.6.8
 sudo pip install pynput==1.6.8 pyinstaller==3.3.1
-apt install -y gnome-shell-extension-dashtodock
