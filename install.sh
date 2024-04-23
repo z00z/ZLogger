355e2086592246eedaf99264e1ed37bbf33023c7
0x0ff537 <diego@zsecurity.org>
2024-04-22 23:38:10 -0400

    Updated to work with python 3

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ rm /var/lib/apt/lists/lock
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install -y wine  
-wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
-wine msiexec /i python-2.7.14.msi
-sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pefile==2019.4.18
-sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.6 pynput==1.6.8
-sudo pip install pynput==1.6.8 pyinstaller==3.3.1
+wget https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe
+wine python-3.12.3-amd64.exe
+sudo wine /root/.wine/drive_c/users/root/AppData/Local/Programs/Python/Python312/python.exe -m pip install pyinstaller pynput
+sudo pip3 install pynput pyinstaller
